b932cfd52e3a7ead6b21032ecf500a28d88703b8
piterenlauda <117058173+piterenlauda@users.noreply.github.com>
2022-11-07 11:29:16 +0800

    Add files via upload

---
+++ Aritmatika.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pertemuan_7;
+
+/**
+ *
+ * @author PC-A-30
+ */
+public class Aritmatika {
+    //property
+    public double hasil;
+    
+    //method tambah
+    public void tambah(double bil1, double bil2){
+        this.hasil = bil1 + bil2;
+    }
+    
+    public void kurang(double bil1, double bil2){
+        this.hasil = bil1 - bil2;
+    }
+    
+    public void kali(double bil1, double bil2){
+        this.hasil = bil1 * bil2;
+    }
+    
+    public void bagi(double bil1, double bil2){
+        this.hasil = bil1 / bil2;
+    }
+}
